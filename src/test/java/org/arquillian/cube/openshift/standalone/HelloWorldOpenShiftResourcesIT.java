bc0b194ca73a98785cbdfed08412de088faf691e
Alex Soto <asotobu@gmail.com>
2018-02-14 16:25:54 +0100

    Initial

---
+++ src/test/java/org/arquillian/cube/openshift/standalone/HelloWorldOpenShiftResourcesIT.java
@@ -0,0 +1,39 @@
+package org.arquillian.cube.openshift.standalone;
+
+import java.io.IOException;
+import java.net.URL;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.arquillian.cube.openshift.api.OpenShiftResource;
+import org.arquillian.cube.openshift.impl.enricher.AwaitRoute;
+import org.arquillian.cube.openshift.impl.enricher.RouteURL;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Arquillian.class)
+@OpenShiftResource("classpath:hello-route.yaml")
+public class HelloWorldOpenShiftResourcesIT {
+
+    @RouteURL("hello-world")
+    @AwaitRoute
+    private URL url;
+
+    @Test
+    public void should_show_hello_world() throws IOException {
+        assertThat(url).isNotNull();
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().get().url(url).build();
+        Response response = okHttpClient.newCall(request).execute();
+
+        assertThat(response).isNotNull();
+        assertThat(response.code()).isEqualTo(200);
+        assertThat(response.body().string()).isEqualTo("Hello OpenShift!\n");
+
+        response.close();
+    }
+
+}
